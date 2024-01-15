1768c00490d101300bfffd2bb9b3efd8b85a3f29
Nishant Racherla <nishant.racherla@gmail.com>
2024-01-15 04:34:41 -0800

    Add capitalize function

--- app/(recipes)/recipes/page.tsx
+++ app/(recipes)/recipes/page.tsx
@@ -1,8 +1,10 @@
+import { capitalize } from "@/lib/utils";
+
 export default function Recipes({
   searchParams,
 }: {
   searchParams?: { [key: string]: string | undefined };
 }) {
   const { filter: filter } = searchParams as { [key: string]: string };
-  return <div>{filter} Recipes</div>;
+  return <div>{capitalize(filter)} Recipes</div>;
 }
