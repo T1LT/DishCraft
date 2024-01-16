7dd27774c0dc673cf939af57c1053bc694ae452f
Nishant Racherla <nishant.racherla@gmail.com>
2024-01-15 20:28:06 -0800

    Remove geist package, import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
