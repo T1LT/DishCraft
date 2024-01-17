9f47d4a2ba6e5b2f850144f60e89217a6a1c573a
Nishant Racherla <nishant.racherla@gmail.com>
2024-01-16 16:19:14 -0800

    Center splash page heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <main className="flex min-h-[calc(100vh-74px)] flex-col items-center justify-center p-4">
         <div>
-          <h1 className="p-2 text-center font-extrabold tracking-tight text-5xl lg:text-7xl">
+          <h1 className="text-center font-extrabold tracking-tight text-5xl lg:text-7xl mb-24">
             What will you cook next?
           </h1>
         </div>
