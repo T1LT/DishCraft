f8e5a67bcbf7de7ac084ba669503f8c4c54a380f
Nishant Racherla <nishant.racherla@gmail.com>
2024-01-20 16:59:29 -0800

    Add search input

--- app/(recipes)/search/page.tsx
+++ app/(recipes)/search/page.tsx
@@ -1,7 +1,12 @@
+import { SearchInput } from "./input";
+
 export default function SearchPage() {
   return (
-    <div>
-      <h1 className="font-bold text-3xl text-center">Search</h1>
+    <div className="w-full max-w-lg space-y-4">
+      <div className="flex flex-col gap-8">
+        <h1 className="font-bold text-3xl text-center">Search</h1>
+        <SearchInput />
+      </div>
     </div>
   );
 }
